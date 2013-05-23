e5644a35854a8372cf39cd68eb2903b1c1477ccc
Jan Ernsting <jan.ernsting@gmail.com>
2013-05-23 18:26:24 +0200

    Remove broken link to adder.sample

--- AdderServiceResteasy/src/de/wwu/pi/RootImpl.java
+++ AdderServiceResteasy/src/de/wwu/pi/RootImpl.java
@@ -11,7 +11,6 @@ public class RootImpl implements Root {
 	public Map<String, Map<String, URI>> links(UriInfo uriInfo) {
 		Map<String, URI> links = new HashMap<>();
 		links.put("adder", adderUri(uriInfo));
-		links.put("adder.sample", adderSampleUri(uriInfo));
 		
 		Map<String, Map<String, URI>> map = new HashMap<>();
 		map.put("links", links);
@@ -22,11 +21,5 @@ public class RootImpl implements Root {
 		return uriInfo.getBaseUriBuilder()
 				.path(AdderResource.class)
 				.build();
-	}
-	
-	private URI adderSampleUri(UriInfo uriInfo) {
-		return uriInfo.getBaseUriBuilder()
-				.path(AdderResource.class, "addTwoNumbers")
-				.build(42, 23);
 	}
 }
