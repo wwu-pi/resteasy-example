aa6ebf0c99285feec2859d0a4e668b6f421df868
Jan Ernsting <jan.ernsting@gmail.com>
2013-05-23 16:44:25 +0200

    Move method to place of first usage

--- AdderServiceResteasy/src/de/wwu/pi/AdderResourceImpl.java
+++ AdderServiceResteasy/src/de/wwu/pi/AdderResourceImpl.java
@@ -11,17 +11,17 @@ public class AdderResourceImpl implements AdderResource {
 		AdderRequest adderRequest = new AdderRequest(42, 23);
 		return Response.seeOther(adderRequestUri(adderRequest)).build();
 	}
+	
+	private URI adderRequestUri(AdderRequest adderRequest) {
+		return UriBuilder.fromResource(AdderResource.class)
+				.segment("" + adderRequest.getFirst())
+				.segment("" + adderRequest.getSecond()).build();
+	}
 
 	public Response addTwoNumbers(AdderRequest adderRequest) {
 		Response response = Response.ok(new AdderResult(adderRequest)).build();
 		response.getMetadata().add("Location", adderRequestUri(adderRequest));
 		return response;
-	}
-
-	private URI adderRequestUri(AdderRequest adderRequest) {
-		return UriBuilder.fromResource(AdderResource.class)
-				.segment("" + adderRequest.getFirst())
-				.segment("" + adderRequest.getSecond()).build();
 	}
 
 	public AdderResult addTwoNumbers(final int first, final int second) {
