a7b8e165abca7d07014f2143fb351dd043c39bb6
Jan Ernsting <jan.ernsting@gmail.com>
2015-04-13 21:21:23 +0200

    Use constant to reference the media type for simple service too

--- SimpleClient/src/de/wwu/pi/RestAdderClient.java
+++ SimpleClient/src/de/wwu/pi/RestAdderClient.java
@@ -4,12 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public interface RestAdderClient {
 
 	@GET
-	@Produces("application/json")
+	@Produces(MediaType.APPLICATION_JSON)
 	public int add(
 			@QueryParam("first") int first,
 			@QueryParam("second") int second);
--- SimpleService/src/de/wwu/pi/AdderResource.java
+++ SimpleService/src/de/wwu/pi/AdderResource.java
@@ -4,12 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class AdderResource {
 
 	@GET
-	@Produces("application/json")
+	@Produces(MediaType.APPLICATION_JSON)
 	public int addTwoNumbers(
 			@QueryParam("first") int first,
 			@QueryParam("second") int second) {
